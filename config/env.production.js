bd5d9105f57ac7bc20f1ad6e2de1bfe0b3b854b7
Ruczkowski, Lukas <lukas.ruczkowski@merck.com>
2016-04-05 09:47:37 +0200

    changed npm run scripts

--- config/env.production.js
+++ config/env.production.js
@@ -3,5 +3,13 @@
 // PRODUCTION CONFIG
 
 module.exports = {
-
+    db: {
+        uri: 'mongodb://localhost/address-book'
+    },
+    authentication: {
+        secret: 'abcd1234'
+    },
+    firebase: {
+        url: 'https://address-book-test.firebaseIO.com'
+    }
 };
